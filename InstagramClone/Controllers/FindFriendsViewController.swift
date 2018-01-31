c2eb7ee051aeb86d246bdc4d12fe603c56428787
Milind Satam <milind.ms14@gmail.com>
2018-01-31 13:22:08 +0530

    TabBarController

---
+++ InstagramClone/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FindFriendsViewController.swift
+//  InstagramClone
+//
+//  Created by MacBookPro on 1/31/18.
+//  Copyright © 2018 basicdas. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ InstagramClone/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  InstagramClone
+//
+//  Created by MacBookPro on 1/31/18.
+//  Copyright © 2018 basicdas. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
